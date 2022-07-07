704376422408714b2ebd8008af4ed9b43849e509
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-07-06 23:49:01 -0300

    feat: hook and state paradigm

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,48 @@
-import './styles.css'
+import "./styles.css";
+
+import { Card } from "../../components/Card";
+
+import React, { useState } from "react";
 
 export function Home() {
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+
+  function handleNewStudent() {
+    const newStudent = {
+      id: students.length+1,
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
   return (
-    <div className='home'>
+    <div className="home">
       <h1>Lista de Presenca</h1>
-      <input type="text" placeholder="Digite o nome..." />
-      <button type="button">Enviar</button>
+      <input 
+        type="text" 
+        placeholder="Digite o nome..." 
+        onChange={e => setStudentName(e.target.value)} 
+      />
+
+      <button type="button" onClick={handleNewStudent}>Adicionar</button>
+
+      {
+        students.map(student => (
+          <Card 
+            id={student.id}
+            key={student.id}
+            name={student.name} 
+            time={student.time} 
+          />
+        ))
+      }
     </div>
   );
 }
